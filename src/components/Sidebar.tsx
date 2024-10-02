68855f1d63d46f056aacedd0cbe03e00b916cf6c
newuzbdeve <jaxongirmirzo2109@gmaile.com>
2024-10-02 14:21:46 +0500

    sidebar fixed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,23 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ChartColumn,
-  ClipboardList,
-  Users2,
-} from "lucide-react";
+import { ChartColumn, ClipboardList, Users2, ChevronLeft } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-const Sidebar: React.FC<{ onToggle: (collapsed: boolean) => void }> = ({
-  onToggle,
-}) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+type SidebarProps = {
+  className?: string;
+};
+
+const Sidebar: React.FC<SidebarProps> = () => {
+  const [isMinimized, setIsMinimized] = useState(false);
   const location = useLocation();
   const path = location.pathname;
 
   const handleToggle = () => {
-    const newCollapsed = !isCollapsed;
-    setIsCollapsed(newCollapsed);
-    onToggle(newCollapsed);
+    setIsMinimized(!isMinimized);
   };
 
   const getLinkClassName = (linkPath: string) =>
-    `flex items-center p-2 rounded-xl hover:bg-primary hover:text-white py-4 ${
+    `flex items-center p-2 rounded-xl hover:bg-primary hover:text-white py-2 ${
       path === linkPath
         ? "bg-primary text-white text-sm"
         : "text-primary text-lg"
@@ -30,37 +25,45 @@ const Sidebar: React.FC<{ onToggle: (collapsed: boolean) => void }> = ({
 
   return (
     <aside
-      className={`fixed top-0 left-0 h-full transition-width duration-300 shadow-md border-r ${
-        isCollapsed ? "w-20" : "w-64"
-      }`}
+      className={cn(
+        `relative hidden h-screen flex-none border-r transition-[width] duration-500 md:block`,
+        !isMinimized ? "w-72" : "w-[70px]"
+      )}
     >
-      <div className="flex items-center justify-between p-4">
-        <button onClick={handleToggle} className="p-2 text-white bg-primary">
-          {isCollapsed ? (
-            <ChevronRightIcon className="w-6 h-6" />
-          ) : (
-            <ChevronLeftIcon className="w-6 h-6" />
-          )}
-        </button>
+      <div className="hidden p-5 pt-10 lg:block">
+        <Link to={"/"}>
+          <img
+            src="https://mbos-landing.vercel.app/_next/image?url=%2Fimages%2Fmbos.png&w=96&q=75"
+            alt="logo"
+          />
+        </Link>
       </div>
+      <ChevronLeft
+        className={cn(
+          "absolute -right-3 top-10 z-50 cursor-pointer rounded-full border bg-background text-3xl text-foreground",
+          isMinimized && "rotate-180"
+        )}
+        onClick={handleToggle}
+      />
+
       <div>
         <ul className="p-3 space-y-2">
           <li>
             <Link to="/" className={getLinkClassName("/")}>
               <ChartColumn className="w-10 h-6" />
-              {!isCollapsed && <span className="text-lg">Asosiy</span>}
+              {!isMinimized && <span className="text-lg">Asosiy</span>}
             </Link>
           </li>
           <li>
             <Link to="/services" className={getLinkClassName("/services")}>
               <ClipboardList className="w-10 h-5" />
-              {!isCollapsed && <span className="text-lg">Xizmatlar</span>}
+              {!isMinimized && <span className="text-lg">Xizmatlar</span>}
             </Link>
           </li>
           <li>
             <Link to="/users" className={getLinkClassName("/users")}>
               <Users2 className="w-10 h-5" />
-              {!isCollapsed && (
+              {!isMinimized && (
                 <span className="text-lg">Foydanaluvchilar</span>
               )}
             </Link>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,26 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar.tsx";
 import Sidebar from "../Sidebar";
-import { useState } from "react";
 
 export const Layout = () => {
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
-  const handleSidebarToggle = (collapsed: boolean) => {
-    setIsSidebarCollapsed(collapsed);
-  };
   return (
     <>
       <div className="flex min-h-screen">
-        <Sidebar onToggle={handleSidebarToggle} />
-        <div
-          className={`flex-1 flex flex-col transition-all duration-300 ${
-            isSidebarCollapsed ? "ml-20" : "ml-64"
-          }`}
-        >
-          <Navbar />
-          <div className="min-h-screen bg-slate-100/80">
-            <Outlet />
-          </div>
+        <Sidebar />
+        <Navbar />
+        <div className="min-h-screen bg-slate-100/80">
+          <Outlet />
         </div>
       </div>
     </>
