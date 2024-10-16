752d686ee8cc994b9c879eec2f269bf6592cf21e
Alisher1221-ru <kr4054658@gmail.com>
2024-10-16 14:36:10 +0500

    fix

--- src/types/clients.ts
+++ src/types/clients.ts
@@ -1,6 +1,11 @@
+import { Contract } from "./contract";
+import { Income } from "./income";
+
 export type Clients = {
   id: number;
   F_I_O: string;
   phone: number;
   adress: string;
+  shartnome?: Contract[];
+  income?: Income[];
 };
