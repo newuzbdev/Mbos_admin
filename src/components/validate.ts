368c5b0732abd4a8540a02dbbff6bd969e1e2984
newuzbdeve <jaxongirmirzo2109@gmaile.com>
2024-10-28 12:05:20 +0500

    fix date errors in contract

--- src/components/validate.ts
+++ src/components/validate.ts
@@ -1,15 +1,26 @@
+// import { z } from "zod";
+
+// export const FormSchema = z
+//   .object({
+//     description: z.string().optional(),
+//     tolash_sana: z.date().optional(),
+//   })
+//   .catchall(
+//     z.union([
+      
+//       z.string().nonempty("Maydon bo'sh qator bo'lishi mumkin emas"),
+//       z.number().min(1, "Maydon ijobiy raqam bo'lishi kerak"),
+//     ])
+//   );
 import { z } from "zod";
 
 export const FormSchema = z
   .object({
     description: z.string().optional(),
-    tolash_sana: z.date().optional(),
-
-    // tolash_sana: z.string().optional(),
+    tolash_sana: z.string().transform((str) => new Date(str)).optional(),
   })
   .catchall(
     z.union([
-      
       z.string().nonempty("Maydon bo'sh qator bo'lishi mumkin emas"),
       z.number().min(1, "Maydon ijobiy raqam bo'lishi kerak"),
     ])
