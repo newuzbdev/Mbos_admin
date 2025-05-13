9f0e74403a5fd1e60600e54aecc9355be87e2f79
unknown <alisherov-cs@gmail.com>
2025-05-13 18:53:58 +0500

    feat: dashboard chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,61 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useGetStatistic } from "@/hooks/dashboard";
 import { TrendingDown, TrendingUp, Users } from "lucide-react";
+import {
+  Bar,
+  BarChart,
+  Legend,
+  Line,
+  LineChart,
+  Rectangle,
+  Tooltip,
+  XAxis,
+} from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const Home = () => {
   const { data: homeStats } = useGetStatistic();
@@ -19,8 +74,7 @@ const Home = () => {
       </div>
       <Tabs defaultValue="overview" className="space-y-4">
         <TabsList>
-          <TabsTrigger value="overview">Umumiy malumotlar
-          </TabsTrigger>
+          <TabsTrigger value="overview">Umumiy malumotlar</TabsTrigger>
         </TabsList>
         <TabsContent value="overview" className="space-y-4">
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -41,7 +95,7 @@ const Home = () => {
             <Card className="text-white bg-primary">
               <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
                 <CardTitle className="text-lg font-medium">
-                Daromadlar
+                  Daromadlar
                 </CardTitle>
                 <TrendingUp className="items-center w-12 h-12 p-2 rounded-lg bg-emerald-400/10" />
               </CardHeader>
@@ -50,17 +104,13 @@ const Home = () => {
                   {homeStats?.data?.data?.income
                     ? formatNumber(homeStats.data.data.income)
                     : 0}
-                  <span className="pl-2 text-base ">
-                    Daromadlar
-                  </span>
+                  <span className="pl-2 text-base ">Daromadlar</span>
                 </div>
               </CardContent>
             </Card>
             <Card className="text-white bg-red-500">
               <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-                <CardTitle className="text-lg font-medium">
-                  Chiqimlar
-                </CardTitle>
+                <CardTitle className="text-lg font-medium">Chiqimlar</CardTitle>
                 <TrendingDown className="items-center w-12 h-12 p-2 text-white rounded-lg bg-red-400/10" />
               </CardHeader>
               <CardContent>
@@ -68,9 +118,7 @@ const Home = () => {
                   {homeStats?.data?.data?.expend
                     ? formatNumber(homeStats.data.data.expend)
                     : 0}
-                  <span className="pl-2 text-base">
-                    Chiqimlar
-                  </span>
+                  <span className="pl-2 text-base">Chiqimlar</span>
                 </div>
               </CardContent>
             </Card>
@@ -90,6 +138,41 @@ const Home = () => {
           </div>
         </TabsContent>
       </Tabs>
+      <div className="mt-4 flex items-center justify-center">
+        <BarChart width={1400} height={500} data={data}>
+          <XAxis dataKey="name" />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="pv"
+            fill="green"
+            activeBar={<Rectangle fill="green" stroke="green" />}
+          />
+          <Bar
+            dataKey="uv"
+            fill="red"
+            activeBar={<Rectangle fill="red" stroke="red" />}
+          />
+          <Bar
+            dataKey="uv"
+            fill="yellow"
+            activeBar={<Rectangle fill="yellow" stroke="yellow" />}
+          />
+        </BarChart>
+      </div>
+      <div className="mt-4 flex items-center justify-center">
+        <LineChart width={1400} height={500} data={data}>
+          <XAxis dataKey="name" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="linear"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </div>
     </div>
   );
 };
